5d092d73163102ec831ae766f20c099d3d5b4d7a
Iris Chen <o0lazybear0o@gmail.com>
2015-06-05 11:35:32 +0800

    QueryServer5.0 : src doc Use CM_1.1 of Team 5; Update fm to version 3.2

---
+++ QueryServer5.0/doc/README.md
@@ -0,0 +1,74 @@
+#Document
+---
+## 系统功能说明
+
++ 提供查询服务
+	+ 用户输入组号，系统查询并返回该组成员名单
+	+ 用户输入姓名，系统查询并返回该人所在小组
++ 系统记录如下数据
+	+ 用户请求次数
+	+ 系统提供服务次数
+	+ 系统拒绝服务次数
++ 生成系统日志
+
+##开发环境依赖
+
++ [CM_1.1.jar](https://github.com/TJSoftwareReuse/DeliverComponents/tree/master/CM/T5/CM_1.1) - Team 5
++ [fm3.2.jar](https://github.com/TJSoftwareReuse/DeliverComponents/tree/master/FM/T2/3.2) - Team 2
++ [PM_v1.2.jar](https://github.com/TJSoftwareReuse/DeliverComponents/tree/master/PM/T9/1.2) - Team 9
++ [License.jar](https://github.com/TJSoftwareReuse/DeliverComponents/tree/master/License/T8/lastest_version) - Team 8
++ log4j-1.2.17.jar 
+
+##系统配置
+
+####配置文件路径
+
++ 配置文件路径为`config.properties`
+
+####配置文件信息
+	
+	DATA_PATH=data.txt #数据文件路径
+	FM_PATH=fm.log #fm构件输出文件路径
+	PM_PATH=pm #pm构件输出文件路径
+	PM_TIME=3000 #pm构件输出间隔时间段
+	LICENSE_NUM=5 # License构件容量
+	SERVER_TYPE=TEAM # 取值为TEAM或MEMBER，查询服务类型
+	
+####修改配置文件
+
++ 直接修改配置信息
++ 修改后不需重启系统
+
+##数据导入
+
+####数据文件路径
+
++ 在配置文件中 `DATA_PATH` 更改数据文件路径
+
+####数据文件信息
+
++ 数据文件格式 `Name:TeamMember\n`
++ 修改数据文件后需重启系统
+
+##系统流程
+
+#### 启动流程
+
+1. 设置配置文件 (如果需要)
+2. 启动系统
+3. 开始查询
+
+#### 查询流程
+
++ `SERVER_TYPE` = `TEAM`
+
+	+ ![TeamQuery](img/TeamQuery.png)
+
++ `SERVER_TYPE` = `MEMBER`
+
+	+ ![MemberQuery](img/MemberQuery.png)
+	+ ![MemberQueryFailed](img/MemberQueryFailed.png)
+	
+#### 日志文件
+
+![fm_log](img/fm_log.png)
---
+++ QueryServer5.0/src/queryserver/QueryServer.java
@@ -0,0 +1,314 @@
+package queryserver;
+
+/**
+ *
+ * @author Iris
+ */
+
+import PerMinute.PMPerMinute;
+import cm.PropertiesUtil;
+
+import com.manager.failure.FailureManager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import src.com.team8.License.License;
+/**
+ *
+ * @author chenqiyuan 
+ */
+
+public class QueryServer implements Runnable{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    private static void load()
+    {
+        File file;
+        
+        QueryServer.dataMtoT = new HashMap<String, String>();
+        QueryServer.dataTtoM = new HashMap<String,Vector<String>>();
+        
+        try {
+        	file = new File(config.readValue("DATA_PATH"));
+        	
+        	if(!file.exists()){
+        		FailureManager.logError("FILE NOT FOUND");
+        		return;
+        	}
+        	dataLastTime = file.lastModified();
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String tmpString = null;
+            while ((tmpString = reader.readLine()) != null){
+                //System.out.print(tmpString + "\n");
+                String[] tmpStr = tmpString.split(":");
+                if (tmpStr.length != 2){
+                    FailureManager.logError("INVAILID INPUT FORMAT");
+                    return;
+                }
+                dataMtoT.put(tmpStr[0], tmpStr[1]);
+                
+                
+                if(dataTtoM.containsKey(tmpStr[1]))
+                {
+                	Vector<String> members;
+                	members = dataTtoM.get(tmpStr[1]);
+                	members.add(tmpStr[0]);
+                	dataTtoM.put(tmpStr[1], members);                	
+                }
+                else
+                {
+                	Vector<String> members = new Vector<String>();
+                	members.add(tmpStr[0]);
+                	dataTtoM.put(tmpStr[1],members);
+                }
+                
+                //System.out.println(tmpStr[0] + " " + tmpStr[1]);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(QueryServer.class.getName()).log(Level.SEVERE, null, ex);            
+            FailureManager.logError("FILE NOT FOUND");
+        } catch (IOException ex) {
+            Logger.getLogger(QueryServer.class.getName()).log(Level.SEVERE, null, ex);
+            FailureManager.logError("INPUT EXCEPTION");
+        }  
+    }
+    
+    public static void readConfig() throws IOException
+    {
+        //config = new ConfigProperties(configPath);
+        config = new PropertiesUtil(configPath);
+        File  file = new File(configPath);
+        configLastTime = file.lastModified();
+    }
+    
+    
+    public static void init() throws IOException{
+        readConfig();
+        currentConfig = config.readAllProperties();
+        try {
+			FailureManager.resetOutputFile(config.readValue("FM_PATH"));
+		} catch (Exception e) {
+			//System.out.println(1);
+			e.printStackTrace();
+		}
+        pm = new PerMinute.PMPerMinute(config.readValue("PM_PATH"));
+        pm.setIntervalTime(Long.parseLong(config.readValue("PM_TIME")));
+        license = new License(Integer.parseInt(config.readValue("LICENSE_NUM")));
+        serverType = config.readValue("SERVER_TYPE");
+        load();
+    }
+    
+    public static void runServer(){
+        FailureManager.logInfo("System start : query "+serverType);
+        pm.start();
+        while (true){
+        	if(serverType.equals("MEMBER"))
+        	{
+	            System.out.print("Please input the name below (Enter 'quit' to quit)\n");
+	            String name = new Scanner(System.in).next().toString();
+	            if (name.equals("quit"))
+	            {
+	            	stop = true;
+	                break;
+	                
+	            }
+	            pm.addIndex("Request", 1);
+	            if (license.inLicense()){
+	                FailureManager.logInfo("Query provided with the name : " + name);
+	                pm.addIndex("RequestProvided", 1);
+	                String team = dataMtoT.get(name);
+	                if (team != null){
+	                    System.out.print("Student " + name + " is in Team " + team + "\n");
+	                } else {
+	                    System.out.print("Student " + name + " cannot be found\n");
+	                }
+	                
+	            } else {
+	                FailureManager.logInfo("Query rejected with the name : " + name);
+	                pm.addIndex("RequestRejected", 1);
+	                System.out.println("Query Reject : Out of license limit.");
+	            }
+        	}
+        	else
+        	if(serverType.equals("TEAM"))
+        	{
+        		System.out.print("Please input the team below (Enter 'quit' to quit)\n");
+                String team = new Scanner(System.in).next().toString();
+                if (team.equals("quit"))
+                {
+                	stop = true;
+                    break;
+                }
+                pm.addIndex("Request", 1);
+                if (license.inLicense()){
+                    FailureManager.logInfo("Query provided with the team : " + team);
+                    pm.addIndex("RequestProvided", 1);
+                    Vector<String> names = dataTtoM.get(team);
+                    if (names != null){
+                        for (String name : names) {                  	
+    						System.out.println(name);
+    					}
+                    	
+                    } else {
+                        System.out.print("Team " + team + " cannot be found\n");
+                    }
+                    
+                } else {
+                    FailureManager.logInfo("Query rejected with the team : " + team);
+                    pm.addIndex("RequestRejected", 1);
+                    System.out.println("Query Reject : Out of license limit.");
+                }
+        	}
+        	else
+        	{
+        		FailureManager.logError("Invalid service parameter. ");
+        		stop = true;
+        		break;
+        	}
+        }
+        pm.stop();
+        System.out.print("Goodbye.\n");
+        FailureManager.logInfo("System shut down");
+    } 
+    
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Runnable#run()
+     */
+    @Override
+    public void run(){
+    	while(!stop)
+    	{
+    		File file = new File(configPath);
+    		if(file.lastModified() != configLastTime)
+    		{
+    			try {
+                            try {
+                                readConfig();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            
+                            if( !config.readValue("DATA_PATH").equals(currentConfig.get("DATA_PATH")))
+                            {
+                                load();
+                                FailureManager.logInfo("Data changes.");
+                            }
+                            if(!config.readValue("FM_PATH").equals(currentConfig.get("FM_PATH")))
+                            {
+                                FailureManager.resetOutputFile(config.readValue("FM_PATH"));
+                                FailureManager.logInfo("Fm path changes to "+config.readValue("FM_PATH"));
+                            }
+                            
+                            if(!config.readValue("PM_PATH").equals(currentConfig.get("PM_PATH")))
+                            {
+                                try {
+                                    pm.setPath(config.readValue("PM_PATH"));
+                                    FailureManager.logInfo("Pm path changes to "+config.readValue("PM_PATH"));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            
+                            if(!config.readValue("LICENSE_NUM").equals(currentConfig.get("LICENSE_NUM")))
+                            {
+                                license = new License(Integer.parseInt(config.readValue("LICENSE_NUM")));
+                                FailureManager.logInfo("License number changes to "+config.readValue("LICENSE_NUM"));
+                            }
+                            
+                            if(!config.readValue("PM_TIME").equals(currentConfig.get("PM_TIME")))
+                            {
+                                pm.setIntervalTime(Long.parseLong(config.readValue("PM_TIME")));
+                                FailureManager.logInfo("Pm time changes to "+config.readValue("PM_TIME"));
+                            }
+                            
+                            if(!config.readValue("SERVER_TYPE").equals(currentConfig.get("SERVER_TYPE")))
+                            {
+                                serverType = config.readValue("SERVER_TYPE");
+                                FailureManager.logInfo("Service type changes to "+config.readValue("SERVER_TYPE"));
+                            }
+                            
+                            
+                            configLastTime = file.lastModified();
+                            currentConfig = config.readAllProperties();
+                        } catch (IOException ex) {
+					Logger.getLogger(QueryServer.class.getName()).log(Level.SEVERE, null, ex);
+				}
+    		}
+    		
+                try {
+                    file = new File(config.readValue("DATA_PATH"));
+                } catch (IOException ex) {
+                    Logger.getLogger(QueryServer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+    		if(file.lastModified() != dataLastTime)
+    		{
+    			load();
+			FailureManager.logInfo("Data changes.");
+    		}	
+    	}
+    	
+    	System.out.println("stoped");
+    	
+    }
+    
+    
+    public static void main(String[] args){
+    	
+    	QueryServer qs = new QueryServer();
+    	thread = new Thread(qs);
+        try {
+			init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        if (dataMtoT != null)
+        {
+        	thread.start();//¿ªÊ¼¼à²âÅäÖÃÊÇ·ñ·¢Éú±ä»¯
+            runServer();
+            
+        }
+        
+    }
+    
+    public static void setconfigPath(String string) {
+		configPath=string;
+		// TODO Auto-generated method stub
+		
+	}
+	public static void setstop(boolean string) {
+		stop=string;
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
+    private static String configPath = "config.properties";
+    private static License license;
+    private static PMPerMinute pm;
+    private static PropertiesUtil config = null;
+    private static HashMap<String, String> dataMtoT;
+    private static HashMap<String,Vector<String>> dataTtoM;
+    private static String serverType;
+    private static Map<String,String> currentConfig;
+    private static Thread thread;
+    private static boolean stop = false;
+    private static long configLastTime;
+    private static long dataLastTime;
+	
+}
